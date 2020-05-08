d9d3cf431c0c1714c045d7cf49ba8ada1ae5d48a
grantfogle <grantjfogle@gmail.com>
2020-05-07 22:50:24 -0600

    mocking functions for api calls

--- components/start/Start.js
+++ components/start/Start.js
@@ -1,10 +1,37 @@
 import React from 'react';
 import { Actions } from 'react-native-router-flux';
 import { StyleSheet, View, Text, TextInput, TouchableOpacity } from 'react-native';
+import { clientId, clientSecret } from '../../secret';
 
 function showCurrentParty() {
     console.log('dog city');
 };
+
+function getAccessToken() {
+    // const credentialsConvertedToBase64 = Basic + btoa(clientId + ":" + clientSecret);
+    // const url = 'https://accounts.spotify.com/api/token';
+    // fetch(url, {
+    //     method: 'POST',
+    //     headers: {
+    //         Authorization: credentialsConvertedToBase64
+    //     },
+    //     body: {
+    //         grant_type: 'client_credentials'
+    //     }
+    // })
+    //     .then(response => response.json())
+    //     .then(data => {
+    //         console.log(data)
+    //         this.searchSongs = data;
+    //     })
+}
+// get auth token
+// create playlist
+// find a playlist
+// find a song
+// add items to to playlist
+// remove items from a playlist
+// get playback info (what's played, current tracks, skip, repeat, pause)
 
 const Start = () => {
     const { container, button, buttonText } = styles;
@@ -16,6 +43,7 @@ const Start = () => {
             <TouchableOpacity style={button} onPress={() => Actions.findPlaylist()}>
                 <Text style={buttonText}>Find a Playlist</Text>
             </TouchableOpacity>
+            {getAccessToken()}
             {/* View Playlist
             Add song to playlist */}
         </View>
