4658d3b1bd09d103fe4043ca0fdb88905476863f
grantfogle <grantjfogle@gmail.com>
2020-05-13 22:26:29 -0600

    added additional spotify calls

--- contexts/OxContext.js
+++ contexts/OxContext.js
@@ -16,7 +16,8 @@ class OxContextProvider extends Component {
         refreshToken: '',
         expirationTime: '',
         playlist: [],
-        songSearch: [],
+        playlistSongs: [],
+        songs: [],
         currentPlaylist: [
             { id: 1, name: 'Flashing Lights', artist: 'Kanye West' },
             { id: 2, name: 'Fly Me to The Moon', artist: 'Frank Sinatra' },
@@ -97,48 +98,29 @@ class OxContextProvider extends Component {
     }
     // create playlist
     async createPlaylist() {
-        this.spotifyApi.createPlaylist(this.state.spotifyUserId, { name: 'party playlist', public: true, collaborative: true })
+        this.spotifyApi.createPlaylist(this.state.spotifyUserId, { name: 'party playlist', public: true, collaborative: true });
     }
-
     // find a playlist
-    // find a song
+    async findAPlaylist() {
 
+    }
+    // get playlist tracks
+    async getPlaylistTracks(playlistId) {
+        this.spotifyApi.getPlaylist(playlistId)
+    }
+    // find a song
+    async searchSongs(query) {
+        const returnedSongs = this.spotifyApi.search(query, ['artist', 'track']);
+        this.setState({ songs: returnedSongs })
+    }
     // add items to to playlist
+    async addTracksToPlaylist(song) {
+        this.spotifyApi.addTracksToPlaylist(this.state.playlistId, [...song]);
+        this.getPlaylistTracks();
+    }
     // remove items from a playlist
     // get playback info (what's played, current tracks, skip, repeat, pause)
 
-    //Refresh Token
-    // refreshTokens = async () => {
-    //     try {
-    //       const credentials = await getSpotifyCredentials() //we wrote this function above
-    //       const credsB64 = btoa(`${credentials.clientId}:${credentials.clientSecret}`);
-    //       const refreshToken = await getUserData('refreshToken');
-    //       const response = await fetch('https://accounts.spotify.com/api/token', {
-    //         method: 'POST',
-    //         headers: {
-    //           Authorization: `Basic ${credsB64}`,
-    //           'Content-Type': 'application/x-www-form-urlencoded',
-    //         },
-    //         body: `grant_type=refresh_token&refresh_token=${refreshToken}`,
-    //       });
-    //       const responseJson = await response.json();
-    //       if (responseJson.error) {
-    //         await getTokens();
-    //       } else {
-    //         const {
-    //           access_token: newAccessToken,
-    //           refresh_token: newRefreshToken,
-    //           expires_in: expiresIn,
-    //         } = responseJson;
-
-    //         const expirationTime = new Date().getTime() + expiresIn * 1000;
-    //         await setUserData('accessToken', newAccessToken);
-    //         if (newRefreshToken) {
-    //           await setUserData('refreshToken', newRefreshToken);
-    //         }
-    //         await setUserData('expirationTime', expirationTime);
-    //     }
-    //   }
     render() {
         return (
             <OxContext.Provider value={{
